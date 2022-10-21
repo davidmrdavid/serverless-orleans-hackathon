97cb8960b8a52b5c064d4a004243245fe63817b7
Sebastian Burckhardt <sburckha@microsoft.com>
2022-10-21 07:20:43 -0700

    fix wrapper so it wraps async also

--- ConnectionTest/Algorithm/StreamWrapper.cs
+++ ConnectionTest/Algorithm/StreamWrapper.cs
@@ -6,6 +6,8 @@ namespace ConnectionTest.Algorithm
      
     using System;
     using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
 
     public class StreamWrapper : Stream
     {
@@ -13,7 +15,9 @@ namespace ConnectionTest.Algorithm
         readonly Dispatcher dispatcher;
         readonly Channel channel;
 
-        public bool Failed { get; private set; }
+        public bool Failed => this.failed == 1;
+
+        volatile int failed = 0;
 
         public StreamWrapper(Stream stream, Dispatcher dispatcher, Channel channel)
         {
@@ -24,10 +28,13 @@ namespace ConnectionTest.Algorithm
 
         void OnFailed()
         {
-            dispatcher.Worker.Submit(new ChannelFailedEvent()
+            if (Interlocked.CompareExchange(ref failed, 1, 0) == 0)
             {
-                Channel = channel,
-            });
+                dispatcher.Worker.Submit(new ChannelFailedEvent()
+                {
+                    Channel = channel,
+                });
+            }      
         }
 
         public override int Read(byte[] buffer, int offset, int count)
@@ -66,7 +73,269 @@ namespace ConnectionTest.Algorithm
             }
         }
 
-        #region Unmodified
+        public override async Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await this.stream.CopyToAsync(destination, bufferSize, cancellationToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                return await this.stream.ReadAsync(buffer, cancellationToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override async Task FlushAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await this.stream.FlushAsync(cancellationToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            try
+            {
+                 return await this.stream.ReadAsync(buffer, offset, count, cancellationToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await this.stream.WriteAsync(buffer, offset, count, cancellationToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await this.stream.WriteAsync(buffer, cancellationToken);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            try
+            {
+                await this.stream.DisposeAsync();
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override void CopyTo(Stream destination, int bufferSize)
+        {
+            try
+            {
+                this.stream.CopyTo(destination, bufferSize);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override void Close()
+        {
+            try
+            {
+                this.stream.Close();
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override int Read(Span<byte> buffer)
+        {
+            try
+            {
+                return this.stream.Read(buffer);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override int ReadByte()
+        {
+            try
+            {
+                return this.stream.ReadByte();
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override void Write(ReadOnlySpan<byte> buffer)
+        {
+            try
+            {
+                this.stream.Write(buffer);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override void WriteByte(byte value)
+        {
+            try
+            {
+                this.stream.WriteByte(value);
+            }
+            catch (ObjectDisposedException)
+            {
+                this.OnFailed();
+                throw;
+            }
+            catch (IOException)
+            {
+                this.OnFailed();
+                throw;
+            }
+        }
+
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            // TODO hoping Orleans does not call these
+            throw new NotImplementedException();
+            //return this.stream.BeginRead(buffer, offset, count, callback, state); 
+        }
+
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            // TODO hoping Orleans does not call these
+            throw new NotImplementedException();
+            //return this.stream.BeginWrite(buffer, offset, count, callback, state); 
+        }
+
+        public override int EndRead(IAsyncResult asyncResult)
+        {
+            return this.stream.EndRead(asyncResult);
+        }
+
+        public override void EndWrite(IAsyncResult asyncResult)
+        {
+            this.stream.EndWrite(asyncResult);
+        }
+
+        public override bool CanTimeout => this.stream.CanTimeout;
+
+        public override int ReadTimeout { get => this.stream.ReadTimeout; set => this.stream.ReadTimeout = value; }
+ 
+        public override int WriteTimeout { get => this.stream.WriteTimeout; set => this.stream.WriteTimeout = value; }
 
         public override bool CanRead => stream.CanRead;
 
@@ -83,7 +352,6 @@ namespace ConnectionTest.Algorithm
             stream.Flush();
         }
 
-
         public override long Seek(long offset, SeekOrigin origin)
         {
             return stream.Seek(offset, origin);
@@ -93,7 +361,5 @@ namespace ConnectionTest.Algorithm
         {
             stream.SetLength(value);
         }
-
-        #endregion
     }
 }
